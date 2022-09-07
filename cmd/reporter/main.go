068bf15e95ff1d02d0d7ec5191cb527d5594d063
viveknathani <viveknathani2402@gmail.com>
2022-09-07 23:09:29 +0530

    chore: cleanup

--- cmd/reporter/main.go
+++ cmd/reporter/main.go
@@ -20,12 +20,9 @@ import (
 	"gonum.org/v1/plot/vg"
 )
 
-var telegramApiKey string = ""
+const MAX_HN_STORIES = 10
 
-type Message struct {
-	ChatID  int64  `json:"chat_id"`
-	Caption string `json:"caption"`
-}
+var telegramApiKey string = ""
 
 func init() {
 	err := godotenv.Load()
@@ -35,7 +32,7 @@ func init() {
 	telegramApiKey = os.Getenv("TELEGRAM_API_KEY")
 }
 
-func getRecords(path string) [][]string {
+func getRecordsFromCSVFile(path string) [][]string {
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -60,7 +57,7 @@ func getUptime(health [][]string) float64 {
 			count++
 		}
 	}
-	result = float64(count) / float64(len(health))
+	result = float64(count) * 100.0 / float64(len(health))
 	return result
 }
 
@@ -101,66 +98,48 @@ func plotMemoryGraph(memory [][]string) {
 		panic(err)
 	}
 
-	// Save the plot to a PNG file.
 	if err := p.Save(4*vg.Inch, 3*vg.Inch, "graph.PNG"); err != nil {
 		panic(err)
 	}
 }
 
-func main() {
+func getTopStoriesFromHN() []interface{} {
 
-	memory := getRecords("/var/memory.txt")
-	health := getRecords("/var/health.txt")
-	uptime := getUptime(health)
-	plotMemoryGraph(memory)
-
-	content := "Hey There!\n"
-	content += fmt.Sprintf("uptime: %f", uptime)
-	content += "%\n"
-
-	res, err := http.Get("https://hacker-news.firebaseio.com/v0/beststories.json")
+	response, err := http.Get("https://hacker-news.firebaseio.com/v0/beststories.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer res.Body.Close()
-	buffer, bufferError := io.ReadAll(res.Body)
-
+	defer response.Body.Close()
+	buffer, bufferError := io.ReadAll(response.Body)
 	if bufferError != nil {
 		log.Fatal(bufferError)
 	}
-
 	var payload interface{}
 	json.Unmarshal(buffer, &payload)
 	data := payload.([]interface{})
+	return data
+}
 
-	for i := 0; i < 10; i++ {
+func getHNStory(id int64) interface{} {
 
-		id := data[i].(float64)
-		api := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", int64(id))
-		res, err := http.Get(api)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer res.Body.Close()
-		buffer, bufferError := io.ReadAll(res.Body)
-
-		if bufferError != nil {
-			log.Fatal(bufferError)
-		}
-
-		var payload interface{}
-		json.Unmarshal(buffer, &payload)
-		storyData := payload
-		ob := storyData.(map[string]interface{})
-		title := ob["title"].(string)
-		url := ob["url"].(string)
-
-		content += fmt.Sprintf("%d. %s - %s\n", i+1, title, url)
+	url := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", id)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer response.Body.Close()
+	buffer, bufferError := io.ReadAll(response.Body)
+	if bufferError != nil {
+		log.Fatal(bufferError)
+	}
+	var payload interface{}
+	json.Unmarshal(buffer, &payload)
+	return payload
+}
+
+func sendToTelegram(content string, photoPath string) {
 
 	telegramHost := fmt.Sprintf("https://api.telegram.org/bot%s", telegramApiKey)
-
 	buf := new(bytes.Buffer)
 	writer := multipart.NewWriter(buf)
 	file, err := os.Open("graph.PNG")
@@ -168,41 +147,59 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
 	writerWithPhoto, err := writer.CreateFormFile("photo", "graph")
 	if err != nil {
 		log.Fatal(err)
 	}
 	io.Copy(writerWithPhoto, file)
-
 	writerWithChat, err := writer.CreateFormField("chat_id")
 	writerWithChat.Write([]byte("5501101308"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	writer.Close()
-
 	_, err = http.Post(telegramHost+"/sendPhoto", writer.FormDataContentType(), buf)
 	if err != nil {
 		log.Fatal(err)
 	}
+	buf = new(bytes.Buffer)
+	writer = multipart.NewWriter(buf)
+	writerWithChat, err = writer.CreateFormField("chat_id")
+	writerWithChat.Write([]byte("5501101308"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	writerWithContent, err := writer.CreateFormField("text")
+	writerWithContent.Write([]byte(content))
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer.Close()
+	_, err = http.Post(telegramHost+"/sendMessage", writer.FormDataContentType(), buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-	messageBuf := new(bytes.Buffer)
-	messageBufWriter := multipart.NewWriter(messageBuf)
-	writerHNWithChat, err := messageBufWriter.CreateFormField("chat_id")
-	writerHNWithChat.Write([]byte("5501101308"))
-	if err != nil {
-		log.Fatal(err)
+func main() {
+
+	memory := getRecordsFromCSVFile("/var/memory.txt")
+	health := getRecordsFromCSVFile("/var/health.txt")
+	uptime := getUptime(health)
+	plotMemoryGraph(memory)
+	data := getTopStoriesFromHN()
+
+	content := "Hey There!\n"
+	content += fmt.Sprintf("uptime: %f", uptime)
+	content += "%\n"
+	for i := 0; i < MAX_HN_STORIES; i++ {
+		id := data[i].(float64)
+		data := getHNStory(int64(id))
+		ob := data.(map[string]interface{})
+		title := ob["title"].(string)
+		url := ob["url"].(string)
+		content += fmt.Sprintf("%d. %s - %s\n", i+1, title, url)
 	}
-	writerWithHN, err := messageBufWriter.CreateFormField("text")
-	writerWithHN.Write([]byte(content))
-	if err != nil {
-		log.Fatal(err)
-	}
-	messageBufWriter.Close()
-	_, err = http.Post(telegramHost+"/sendMessage", messageBufWriter.FormDataContentType(), messageBuf)
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	sendToTelegram(content, "graph.PNG")
 }
