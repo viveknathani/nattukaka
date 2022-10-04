9b663964eaa37b12366ba58b739f91c6fab109a1
viveknathani <viveknathani2402@gmail.com>
2022-10-04 22:37:46 +0530

    fix: setting header after writeheader has no effect

--- httpkaka/404.go
+++ httpkaka/404.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (s *Server) serve404(w http.ResponseWriter, r *http.Request) {
 
-	w.WriteHeader(http.StatusNotFound)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte("<h3>oops, 404!</h3>"))
 }
