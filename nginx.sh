9bce9e21aa9e364744049cf305ab4251e60e2da8
viveknathani <viveknathani2402@gmail.com>
2024-07-26 14:01:30 +0530

    fix: root path

--- nginx.sh
+++ nginx.sh
@@ -1,2 +1,7 @@
 sudo apt update
 sudo apt install nginx
+
+sudo cp ~/nattukaka/services/nattukaka.dev/nginx.conf /etc/nginx/sites-available/nattukaka.dev
+sudo ln -s /etc/nginx/sites-available/nattukaka.dev /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl restart nginx
