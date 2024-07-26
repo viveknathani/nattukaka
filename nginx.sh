f1020014317b642ff9c569c4a2faf716c4619261
viveknathani <viveknathani2402@gmail.com>
2024-07-26 14:56:45 +0530

    chore: add nginx for tym

--- nginx.sh
+++ nginx.sh
@@ -3,5 +3,9 @@ sudo apt install nginx
 
 sudo cp ~/nattukaka/services/nattukaka.dev/nginx.conf /etc/nginx/sites-available/nattukaka.dev
 sudo ln -s /etc/nginx/sites-available/nattukaka.dev /etc/nginx/sites-enabled/
+
+sudo cp ~/nattukaka/services/teachyourselfmath/nginx.conf /etc/nginx/sites-available/teachyourselfmath
+sudo ln -s /etc/nginx/sites-available/teachyourselfmath /etc/nginx/sites-enabled/
+
 sudo nginx -t
 sudo systemctl restart nginx
