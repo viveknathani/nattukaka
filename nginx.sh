ff4b93e4b7f706265e2e938c654d0cd4f24a0401
viveknathani <viveknathani2402@gmail.com>
2024-07-26 15:49:44 +0530

    chore: cleanup, add vivekn.dev

--- nginx.sh
+++ nginx.sh
@@ -1,11 +1,19 @@
 sudo apt update
 sudo apt install nginx
 
+
+sudo mkdir -p /var/www/nattukaka.dev
+sudo cp /root/nattukaka/services/nattukaka.dev/index.html /var/www/nattukaka.dev/
+sudo chmod -R 755 /var/www/nattukaka.dev
+sudo chown -R www-data:www-data /var/www/nattukaka.dev
 sudo cp ~/nattukaka/services/nattukaka.dev/nginx.conf /etc/nginx/sites-available/nattukaka.dev
 sudo ln -s /etc/nginx/sites-available/nattukaka.dev /etc/nginx/sites-enabled/
 
 sudo cp ~/nattukaka/services/teachyourselfmath/nginx.conf /etc/nginx/sites-available/teachyourselfmath
 sudo ln -s /etc/nginx/sites-available/teachyourselfmath /etc/nginx/sites-enabled/
+
+sudo cp ~/nattukaka/services/vivekn.dev/nginx.conf /etc/nginx/sites-available/vivekn.dev
+sudo ln -s /etc/nginx/sites-available/vivekn.dev /etc/nginx/sites-enabled/
 
 sudo nginx -t
 sudo systemctl restart nginx
