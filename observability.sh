33574a344913e65424f6de6f705bed71e03130cd
viveknathani <viveknathani2402@gmail.com>
2024-07-27 17:32:17 +0530

    chore: node exporter

--- observability.sh
+++ observability.sh
@@ -4,11 +4,10 @@ docker network create nattukaka-network
 docker run -d --name=grafana -p 3000:3000 --network nattukaka-network grafana/grafana
 docker run -d --name=loki -p 3100:3100 --network nattukaka-network grafana/loki
 docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
-docker run -d --name=cadvisor -p 3200:3200 \
-  --mount type=bind,source=/,target=/rootfs:ro \
-  --mount type=bind,source=/var/run,destination=/var/run:ro \
+docker run -d --name=node_exporter \
+  -p 9200:9200 \
   --network nattukaka-network \
-  google/cadvisor:latest
+  prom/node-exporter
 docker run -d --name=prometheus -p 9090:9090 \
   -v /root/nattukaka/prometheus.yml:/etc/prometheus/prometheus.yml \
   --network nattukaka-network \
