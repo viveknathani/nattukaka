4f2df536afa9c35adfdbae89ad2b1d44c01c466e
viveknathani <viveknathani2402@gmail.com>
2025-01-19 18:51:15 +0530

    chore: deployment store

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,54 @@ use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Json;
 use axum::Router;
+use chrono::Utc;
 use log::info;
 use log::error;
+use serde::Deserialize;
+use serde::Serialize;
+use serde::Serializer;
 use serde_json::json;
 use serde_json::Value;
+use redis::Commands;
 use std::env;
 use std::process::exit;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum DeploymentStatus {
+  NotAvailable,
+  InProgress,
+  Success,
+  Failed
+}
+
+fn serialize_deployment_status<S>(status: &DeploymentStatus, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&status.to_string())
+}
+
+
+impl DeploymentStatus {
+  fn to_string(&self) -> &str {
+    match self {
+      DeploymentStatus::NotAvailable => "NOT_AVAILABLE",
+      DeploymentStatus::InProgress => "IN_PROGRESS",
+      DeploymentStatus::Success => "SUCCESS",
+      DeploymentStatus::Failed => "FAILED",
+    }
+  }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Deployment {
+  service: String,
+
+  #[serde(serialize_with = "serialize_deployment_status")]
+  status: DeploymentStatus,
+  
+  timestamp: String,
+}
 
 fn send_standardized_response(
   code: StatusCode,
@@ -67,8 +109,62 @@ async fn main() {
         },
     }
 
-    let port = env::var("PORT").unwrap_or("8084".to_string());
+    const SERVICE_COUNT: usize = 3;
+    const REDIS_KEYS_PREFIX_NATTUKAKA_SERVICE: &str = "NATTUKAKA:DEPLOYMENTS";
+    let port = env::var("PORT").unwrap_or_else(|_| "8084".to_string());
     let _api_key = env::var("NATTUKAKA_API_KEY").unwrap_or("NATTUKAKA_API_KEY_NOT_SET".to_string());
+    let redis_url = env::var("REDIS_URL").unwrap_or("REDIS_URL_NOT_SET".to_string());
+    let redis_client = match redis::Client::open(redis_url) {
+      Ok(client) => client,
+      Err(err) => {
+        error!("could not get a client for redis: {}", err);
+        exit(1);
+      }
+    };
+    let mut redis_conn = match redis_client.get_connection() {
+      Ok(conn) => conn,
+      Err(err) => {
+        error!("could not connect with redis: {}", err);
+        exit(1);
+      }
+    };
+    let services: [&str; SERVICE_COUNT] = ["teachyourselfmath", "vivekn.dev", "workdiff"];
+
+    for service in services {
+      let redis_key = format!("{}:{}", REDIS_KEYS_PREFIX_NATTUKAKA_SERVICE, service);
+      let out: Option<String> = match redis_conn.get(&redis_key) {
+        Ok(val) => val,
+        Err(err) => {
+          error!("failed to get service info for {} with error: {}", service, err);
+          exit(1);
+        }
+      };
+      if out.is_none() {
+        let deployment = Deployment {
+          service: (&service).to_string(),
+          status: DeploymentStatus::NotAvailable,
+          timestamp:  Utc::now().to_rfc3339(),
+        };
+
+        let deployment_str = match serde_json::to_string(&deployment) {
+          Ok(result) => result,
+          Err(err) => {
+            error!("failed to serialize for {:?} with error: {}", deployment, err);
+            exit(1);
+          }
+        };
+
+        let _ : Option<String> =  match redis_conn.set(&redis_key, deployment_str) {
+          Ok(val) => val,
+          Err(err) => {
+            error!("redis set for {} failed {}", service, err);
+            exit(1);
+          }
+        };
+      }
+      info!("service info for {} is set", service);
+    }
+    
 
     let app: Router = Router::new()
         .route("/", get(hello))
