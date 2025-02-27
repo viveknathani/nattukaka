4aa40e97ccf1a7b8e429fa725b29dc6ec4ea0713
viveknathani <viveknathani2402@gmail.com>
2025-02-27 19:20:10 +0530

    chore: cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,7 +11,6 @@ sudo ufw allow https
 sudo ufw enable
 sudo ufw status
 sudo apt install fail2ban
-sudo ufw allow from 192.168.1.0/24 to any port 8080:9000 proto tcp
 
 # docker
 sudo apt-get update
@@ -38,7 +37,6 @@ sudo -u viveknathani psql # verify that this is working by \conninfo
 exit
 # replace 16 with your version number (if needed)
 sudo vi /etc/postgresql/16/main/postgresql.conf # change -> listen_addresses = '*'
-sudo ufw allow 6379/tcp
 sudo service postgresql restart
 
 # redis
@@ -47,17 +45,3 @@ sudo apt install redis-server
 sudo vi /etc/redis/redis.conf # change -> 1) requirepass your_secure_password_here 2) bind 0.0.0.0 3) maxmemory-policy noeviction
 sudo ufw allow 6379/tcp
 sudo service redis-server restart
-
-# clickhouse
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
-curl -fsSL 'https://packages.clickhouse.com/rpm/lts/repodata/repomd.xml.key' | sudo gpg --dearmor -o /usr/share/keyrings/clickhouse-keyring.gpg
-
-ARCH=$(dpkg --print-architecture)
-echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg arch=${ARCH}] https://packages.clickhouse.com/deb stable main" | sudo tee /etc/apt/sources.list.d/clickhouse.list
-sudo apt-get update
-sudo apt-get install -y clickhouse-server clickhouse-client
-sudo ufw allow 9000/tcp
-sudo ufw allow 8123/tcp
-
-echo "NOTE: add the backup script for sixhuman"
-
