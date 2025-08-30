57f9f7148779ba33a1e6968738ca2207808f142d
viveknathani <viveknathani2402@gmail.com>
2025-08-30 16:24:48 +0530

    chore: go back to the minimal layer

--- docs/TODO.md
+++
@@ -1,5 +0,0 @@
-1. ui
-2. vps
-3. docs and refactoring
-4. blog
-5. publish
--- joystick/database/migrations/20250625150526_create-users-table.up.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists users (
-  id serial primary key,
-  uuid uuid default gen_random_uuid() not null unique,
-  username varchar(255) not null unique,
-  password bytea not null
-)
