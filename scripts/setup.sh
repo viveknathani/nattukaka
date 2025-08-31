bb7acee4e8cd53c971cc6224d0b2bb2341dada44
viveknathani <viveknathani2402@gmail.com>
2025-08-31 11:06:03 +0530

    feat: rotate_keys, cleaner setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,6 +12,10 @@ sudo ufw enable
 sudo ufw status
 sudo apt install fail2ban
 
+# automatic updates
+sudo apt install unattended-upgrades
+sudo dpkg-reconfigure unattended-upgrades
+
 # docker
 sudo apt-get update
 sudo apt-get install ca-certificates curl
@@ -24,24 +28,3 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# postgresql
-sudo apt install postgresql postgresql-contrib
-sudo systemctl start postgresql.service
-sudo -i -u postgres
-createuser --interactive # add viveknathani here
-createdb viveknathani
-exit
-sudo adduser viveknathani
-sudo -u viveknathani psql # verify that this is working by \conninfo
-exit
-# replace 16 with your version number (if needed)
-sudo vi /etc/postgresql/16/main/postgresql.conf # change -> listen_addresses = '*'
-sudo service postgresql restart
-
-# redis
-sudo apt update
-sudo apt install redis-server
-sudo vi /etc/redis/redis.conf # change -> 1) requirepass your_secure_password_here 2) bind 0.0.0.0 3) maxmemory-policy noeviction
-sudo ufw allow 6379/tcp
-sudo service redis-server restart
