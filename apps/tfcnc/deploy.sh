0e08283e081357f2f189811c18729690e979f62c
viveknathani <viveknathani2402@gmail.com>
2025-10-23 19:49:18 +0530

    fix: naming

--- apps/tfcnc/deploy.sh
+++ apps/tfcnc/deploy.sh
@@ -3,7 +3,7 @@
 # constants
 PORT=8086
 IMAGE_NAME="tfcnc"
-DIRECTORY_NAME="tfcnc"
+DIRECTORY_NAME="tradeforces"
 CONTAINER_NAME="tfcnc"
 ENV_FILE_PATH=~/environments/tfcnc
 
@@ -18,14 +18,14 @@ fi
 # prepare directory
 cd ~/services/
 rm -rf $DIRECTORY_NAME
-git clone https://github.com/viveknathani/tradeforces.git
+git clone git@github.com:viveknathani/tradeforces.git
 cd $DIRECTORY_NAME
 cp $ENV_FILE_PATH .env
 
 # docker!
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
-docker build -t tfcnc -f cnc.Dockerfile
+docker build -t tfcnc -f cnc.Dockerfile .
 docker run -d --label tfcnc=latest \
     --name=$CONTAINER_NAME \
     --network host \
